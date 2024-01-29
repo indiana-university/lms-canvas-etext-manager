d4940513e93e7aec951cd934bcc5bcbc28b0f244
Gregory J Thomas <gjthomas@iu.edu>
2024-01-29 14:56:33 -0500

    Initial commit

---
+++ src/main/java/edu/iu/uits/lms/microservicestemplate/config/SecurityConfig.java
@@ -0,0 +1,113 @@
+package edu.iu.uits.lms.microservicestemplate.config;
+
+import edu.iu.uits.lms.common.it12logging.LmsFilterSecurityInterceptorObjectPostProcessor;
+import edu.iu.uits.lms.common.it12logging.RestSecurityLoggingConfig;
+import edu.iu.uits.lms.common.oauth.CustomJwtAuthenticationConverter;
+import edu.iu.uits.lms.lti.service.LmsDefaultGrantedAuthoritiesMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
+import uk.ac.ox.ctl.lti13.Lti13Configurer;
+
+import static edu.iu.uits.lms.lti.LTIConstants.BASE_USER_ROLE;
+import static edu.iu.uits.lms.lti.LTIConstants.WELL_KNOWN_ALL;
+
+@Configuration
+public class SecurityConfig {
+
+    @Configuration
+    @Order(SecurityProperties.BASIC_AUTH_ORDER - 5)
+    public static class RestSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.cors().and()
+                    .requestMatchers().antMatchers("/rest/**", "/api/**")
+                    .and()
+                    .authorizeRequests()
+                    // In order to allow CORS preflight requests to succeed, we need to allow OPTIONS requests to the token endpoint
+                    .antMatchers("/rest/**")
+                    .access("hasAuthority('SCOPE_lms:rest') and hasAuthority('ROLE_LMS_REST_ADMINS')")
+                    .antMatchers("/api/**").permitAll()
+                    .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                    .and()
+                    .oauth2ResourceServer()
+                    .jwt().jwtAuthenticationConverter(new CustomJwtAuthenticationConverter());
+
+            http.apply(new RestSecurityLoggingConfig());
+        }
+    }
+
+    @Configuration
+    @Order(SecurityProperties.BASIC_AUTH_ORDER - 4)
+    public static class AppWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private LmsDefaultGrantedAuthoritiesMapper lmsDefaultGrantedAuthoritiesMapper;
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .requestMatchers()
+                    .antMatchers(WELL_KNOWN_ALL, "/error", "/app/**")
+                    .and()
+                    .authorizeRequests()
+                    .antMatchers(WELL_KNOWN_ALL, "/error").permitAll()
+                    .antMatchers("/**").hasRole(BASE_USER_ROLE)
+                    .withObjectPostProcessor(new LmsFilterSecurityInterceptorObjectPostProcessor())
+                    .and()
+                    .headers()
+                    .contentSecurityPolicy("style-src 'self' 'unsafe-inline'; form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
+                    .and()
+                    .referrerPolicy(referrer -> referrer
+                            .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
+
+            //Setup the LTI handshake
+            Lti13Configurer lti13Configurer = new Lti13Configurer()
+                    .grantedAuthoritiesMapper(lmsDefaultGrantedAuthoritiesMapper);
+
+            http.apply(lti13Configurer);
+
+            //Fallback for everything else
+            http.requestMatchers().antMatchers("/**")
+                    .and()
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .withObjectPostProcessor(new LmsFilterSecurityInterceptorObjectPostProcessor())
+                    .and()
+                    .headers()
+                    .contentSecurityPolicy("style-src 'self' 'unsafe-inline'; form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
+                    .and()
+                    .referrerPolicy(referrer -> referrer
+                            .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
+        }
+
+        @Override
+        public void configure(WebSecurity web) throws Exception {
+            // ignore everything except paths specified
+            web.ignoring().antMatchers("/app/jsrivet/**", "/app/webjars/**", "/app/css/**", "/app/js/**", "/favicon.ico");
+        }
+
+    }
+
+
+    @Configuration
+    @Order(SecurityProperties.BASIC_AUTH_ORDER - 2)
+    public static class CatchAllSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            http.requestMatchers().antMatchers("/**")
+                    .and()
+                    .authorizeRequests()
+                    .anyRequest().authenticated();
+        }
+    }
+}
---
+++ src/test/java/edu/iu/uits/lms/microservicestemplate/services/AppLaunchSecurityTest.java
@@ -0,0 +1,89 @@
+package edu.iu.uits.lms.microservicestemplate.services;
+
+import edu.iu.uits.lms.lti.LTIConstants;
+import edu.iu.uits.lms.lti.config.TestUtils;
+import edu.iu.uits.lms.microservicestemplate.config.ToolConfig;
+import edu.iu.uits.lms.microservicestemplate.controller.ToolController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import uk.ac.ox.ctl.lti13.security.oauth2.client.lti.authentication.OidcAuthenticationToken;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(value = ToolController.class, properties = {"oauth.tokenprovider.url=http://foo"})
+@Import(ToolConfig.class)
+@ActiveProfiles("none")
+public class AppLaunchSecurityTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void appNoAuthnLaunch() throws Exception {
+        //This is a secured endpoint and should not allow access without authn
+        mvc.perform(get("/app/index/1234")
+                        .header(HttpHeaders.USER_AGENT, TestUtils.defaultUseragent())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void appAuthnWrongContextLaunch() throws Exception {
+        OidcAuthenticationToken token = TestUtils.buildToken("userId", "asdf", LTIConstants.BASE_USER_ROLE);
+
+        SecurityContextHolder.getContext().setAuthentication(token);
+
+        //This is a secured endpoint and should not allow access without authn
+        ResultActions mockMvcAction = mvc.perform(get("/app/index/1234")
+                .header(HttpHeaders.USER_AGENT, TestUtils.defaultUseragent())
+                .contentType(MediaType.APPLICATION_JSON));
+
+        mockMvcAction.andExpect(status().isInternalServerError());
+        mockMvcAction.andExpect(MockMvcResultMatchers.view().name("error"));
+        mockMvcAction.andExpect(MockMvcResultMatchers.model().attributeExists("error"));
+    }
+
+    @Test
+    public void appAuthnLaunch() throws Exception {
+        OidcAuthenticationToken token = TestUtils.buildToken("userId", "1234", LTIConstants.BASE_USER_ROLE);
+
+        SecurityContextHolder.getContext().setAuthentication(token);
+
+        //This is a secured endpoint and should not allow access without authn
+        mvc.perform(get("/app/index/1234")
+                        .header(HttpHeaders.USER_AGENT, TestUtils.defaultUseragent())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void randomUrlNoAuth() throws Exception {
+        //This is a secured endpoint and should not allow access without authn
+        mvc.perform(get("/asdf/foobar")
+                        .header(HttpHeaders.USER_AGENT, TestUtils.defaultUseragent())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    public void randomUrlWithAuth() throws Exception {
+        OidcAuthenticationToken token = TestUtils.buildToken("userId", "1234", LTIConstants.BASE_USER_ROLE);
+        SecurityContextHolder.getContext().setAuthentication(token);
+
+        //This is a secured endpoint and should not allow access without authn
+        mvc.perform(get("/asdf/foobar")
+                        .header(HttpHeaders.USER_AGENT, TestUtils.defaultUseragent())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+}
