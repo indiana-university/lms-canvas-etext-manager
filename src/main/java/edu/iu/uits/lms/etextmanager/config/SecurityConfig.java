9aef503e03826149a8a8edafcbe8b8dc902b0abf
Damian Sobieralski <dsobiera@iu.edu>
2025-01-15 12:07:09 -0500

    LMSA-9589 - removed comments

--- src/main/java/edu/iu/uits/lms/etextmanager/config/SecurityConfig.java
+++ src/main/java/edu/iu/uits/lms/etextmanager/config/SecurityConfig.java
@@ -75,32 +75,6 @@ public class SecurityConfig {
         return http.build();
     }
 
-
-//    @Configuration
-//    @Order(SecurityProperties.BASIC_AUTH_ORDER - 5)
-//    public static class AppRestSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-//
-//        @Override
-//        public void configure(HttpSecurity http) throws Exception {
-//            http
-//                    .cors().and()
-//                    .requestMatchers().antMatchers("/rest/**", "/api/**")
-//                    .and()
-//                    .authorizeRequests()
-//                    .antMatchers("/rest/**")
-//                    .access("hasAuthority('SCOPE_lms:rest') and hasAuthority('ROLE_LMS_REST_ADMINS')")
-//                    .antMatchers("/api/**").permitAll()
-//                    .and()
-//                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                    .and()
-//                    .oauth2ResourceServer()
-//                    .jwt().jwtAuthenticationConverter(new CustomJwtAuthenticationConverter());
-//
-//            http.apply(new RestSecurityLoggingConfig());
-//        }
-//    }
-//
-
     @Bean
     @Order(6)
     public SecurityFilterChain appFilterChain(HttpSecurity http) throws Exception {
@@ -117,7 +91,6 @@ public class SecurityConfig {
                 );
         return http.build();
     }
-
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -151,82 +124,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-
-//    @Configuration
-//    @Order(SecurityProperties.BASIC_AUTH_ORDER - 4)
-//    public static class AppWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-//
-//        @Autowired
-//        private DefaultInstructorRoleRepository defaultInstructorRoleRepository;
-//
-//        @Autowired
-//        private ETextService eTextUserService;
-//
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http
-//                    .requestMatchers()
-//                    .antMatchers(WELL_KNOWN_ALL, "/error", "/app/**")
-//                    .and()
-//                    .authorizeRequests()
-//                    .antMatchers(WELL_KNOWN_ALL, "/error").permitAll()
-//                    .antMatchers("/**").hasRole(BASE_USER_ROLE)
-//                    .withObjectPostProcessor(new LmsFilterSecurityInterceptorObjectPostProcessor())
-//                    .and()
-//                    .headers()
-//                    .contentSecurityPolicy("style-src 'self' 'unsafe-inline'; form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
-//                    .and()
-//                    .referrerPolicy(referrer -> referrer
-//                            .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
-//
-//
-//            //Setup the LTI handshake
-//            Lti13Configurer lti13Configurer = new Lti13Configurer()
-//                    .grantedAuthoritiesMapper(new CustomRoleMapper(defaultInstructorRoleRepository, eTextUserService));
-//
-//
-//            http.apply(lti13Configurer);
-//
-//            //Fallback for everything else
-//            http.requestMatchers().antMatchers("/**")
-//                    .and()
-//                    .authorizeRequests()
-//                    .anyRequest().authenticated()
-//                    .withObjectPostProcessor(new LmsFilterSecurityInterceptorObjectPostProcessor())
-//                    .and()
-//                    .headers()
-//                    .contentSecurityPolicy("style-src 'self' 'unsafe-inline'; form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
-//                    .and()
-//                    .referrerPolicy(referrer -> referrer
-//                            .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
-//        }
-//
-//        @Override
-//        public void configure(WebSecurity web) throws Exception {
-//            // ignore everything except paths specified
-//            web.ignoring().antMatchers("/app/jsrivet/**", "/app/webjars/**", "/app/css/**", "/app/js/**", "/favicon.ico");
-//        }
-//
-//    }
-//
-//    @Configuration
-//    @Order(SecurityProperties.BASIC_AUTH_ORDER - 2)
-//    public static class CatchAllSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//        @Override
-//        public void configure(HttpSecurity http) throws Exception {
-//            http.requestMatchers().antMatchers("/**")
-//                    .and()
-//                    .authorizeRequests()
-//                    .anyRequest().authenticated()
-//                    .withObjectPostProcessor(new LmsFilterSecurityInterceptorObjectPostProcessor())
-//                    .and()
-//                    .headers()
-//                    .contentSecurityPolicy("style-src 'self' 'unsafe-inline'; form-action 'self'; frame-ancestors 'self' https://*.instructure.com")
-//                    .and()
-//                    .referrerPolicy(referrer -> referrer
-//                            .policy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN));
-//        }
-//    }
 }
